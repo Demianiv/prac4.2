8a45d07843884e2f86ad3340dc63541efe53c8a7
Demianiv <160580388+Demianiv@users.noreply.github.com>
2024-04-23 23:50:05 +0300

    prac4.2

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let myImage = document.querySelector('img');
+
+myImage .onclick = function()
+{
+    let mySrc = myImage .getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png')
+    {
+myImage .setAttribute('src', 'images/firefox2.png');
+    }
+    elese 
+    {
+        myImage .setAttribute('src', 'images/firefox-icon.png')
+    }
+}
